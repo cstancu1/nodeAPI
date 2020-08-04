06bd3677aa0ea000beea14da03965dc7520d0290
Stancu Claudiu Emil <32159498+cstancu1@users.noreply.github.com>
2020-08-04 14:49:44 +0300

    Update config.js

--- modules/database/config.js
+++ modules/database/config.js
@@ -1,9 +1,8 @@
 const credentials={
     user: 'postgres',
-    password: '!@#Energie22',
+    password: '',
     host: '127.0.0.1',
     database: 'teamit',
-    password: '!@#Energie22',
     port: 5432
 }
 
